21d5fa101378380bb204e834f97998a44b15a51c
unknown <rijal.1344@gmail.com>
2024-10-01 09:38:51 +0700

    feat: register

--- src/model/register-model.ts
+++ src/model/register-model.ts
@@ -1,5 +1,23 @@
+import type { user } from "@prisma/client";
+
 export interface RegisterRequest {
   name: string;
   email: string;
   password: string;
+}
+
+export interface RegisterResponse {
+  name: string;
+  email: string;
+  created_at: Date;
+  updated_at: Date;
+}
+
+export function toRegisterResponse(user: user): RegisterResponse {
+  return {
+    name: user.name,
+    email: user.email,
+    created_at: user.created_at,
+    updated_at: user.updated_at,
+  };
 }
--- src/service/register-service.ts
+++ src/service/register-service.ts
@@ -1,12 +1,37 @@
-import type { RegisterRequest } from "../model/register-model";
+import { prisma } from "../app/database";
+import { ErrorResponse } from "../error/error-response";
+import {
+  toRegisterResponse,
+  type RegisterRequest,
+  type RegisterResponse,
+} from "../model/register-model";
+import { User } from "../utils/user";
 import { RegisterValidation } from "../validation/register-validation";
 import { Validation } from "../validation/validation";
 
 export class RegisterService {
-  static async register(request: RegisterRequest): Promise<any> {
+  static async register(request: RegisterRequest): Promise<RegisterResponse> {
     const requestBody: RegisterRequest = Validation.validate(
       RegisterValidation.registerRequest,
       request
     );
+
+    const isEmailExist = await User.isEmailExist(requestBody);
+
+    if (isEmailExist) {
+      throw new ErrorResponse(404, "email already exist");
+    }
+
+    const [user] = await prisma.$transaction([
+      prisma.user.create({
+        data: {
+          name: requestBody.name,
+          email: requestBody.email,
+          password: Bun.password.hashSync(requestBody.password),
+        },
+      }),
+    ]);
+
+    return toRegisterResponse(user);
   }
 }
