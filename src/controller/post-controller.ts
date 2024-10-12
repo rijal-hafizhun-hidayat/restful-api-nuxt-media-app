51f116eac59f9a0c635746bcb482019e47a31e24
unknown <rijal.1344@gmail.com>
2024-10-12 17:41:02 +0700

    feat: add read and store post user

--- src/controller/post-controller.ts
+++ src/controller/post-controller.ts
@@ -1,14 +1,37 @@
 import type { NextFunction, Request, Response } from "express";
+import { PostService } from "../service/post-service";
+import type { PostRequest } from "../model/post-model";
 
 export class PostController {
-  static async getAll(
+  static async getAllByUserId(
     req: Request,
     res: Response,
     next: NextFunction
   ): Promise<any> {
     try {
+      const userId: number = (req as any).currentUser.id;
+      const result = await PostService.getAllByUserId(userId);
       return res.status(200).json({
-        data: true,
+        data: result,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async storePostByUserId(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<any> {
+    try {
+      const request: PostRequest = req.body as PostRequest;
+      const userId: number = (req as any).currentUser.id;
+      const result = await PostService.storePostByUserId(request, userId);
+      return res.status(200).json({
+        statusCode: 200,
+        message: "success create post",
+        data: result,
       });
     } catch (error) {
       next(error);
